4430cdf351be983517680be63ec6361ac57c5d03
fati-ma <fatima.atiyya@yahoo.com>
2021-02-14 13:09:28 +0200

    bfs-graph

---
+++ challenges/breadthFirst/README.md
@@ -0,0 +1,13 @@
+# Breadth-First Traversal of a Graph
+
+## Challenge
+Implement a breadth-first traversal on a graph.
+
+## Approach & Efficiency
+
+I created a breadth-first traversal method that accepts a starting node that returns a collection of nodes in the order they were visited, then displayed the collection.
+
+
+Time: O(n^2)
+
+Space: O(n)
---
+++ challenges/breadthFirst/breadth-first.js
@@ -0,0 +1,80 @@
+'use strict';
+
+class Vertex {
+    constructor(value) {
+        this.value = value;
+    }
+}
+
+class Edge {
+    constructor(vertex, weight) {
+        this.vertex = vertex;
+        this.weight = weight;
+    }
+}
+
+class Graph {
+    constructor() {
+        this.adjacencyList = new Map();
+        // this.size = 0; //Can add this here and increment it every time a vertex is added to maintain the size attribute 
+    }
+
+    //add new vertex to this graph
+    addVertex(vertex) {
+        //.set(key,value) puts a key/value aka node in the map
+        this.adjacencyList.set(vertex, []); // when add vertex for first time the value is an empty array because it will eventually hold a list of all of the nodes that it is connected to
+    }
+
+    addDirectedEdge(startVertex, endVertex, weight) {
+        //error checking, must have starting and ending node
+        if (!this.adjacencyList.has(startVertex) || (!this.adjacencyList.has(endVertex))) { //map.has() returns bool: true if exists in map, false if not
+            throw new Error('error: start or end vertex missing');
+        }
+        //get startVertex from Map (all of verticies)
+        //push a new Edge that will connect the start to the end 
+        const adjacencies = this.adjacencyList.get(startVertex); //map.get() returns [] of all adjacencies
+        adjacencies.push(new Edge(endVertex, weight));
+    }
+
+    getNeighbors(vertex) {
+        if (!this.adjacencyList.has(vertex)) {
+            throw new Error('error - invalid vertex', vertex);
+        }
+        return this.adjacencyList.get(vertex); //returns an array
+    }
+
+    //breadthFirstSearch
+    bfs(startNode) {
+
+        if (!startNode) return null;
+
+        const queue = [];
+        // a set is an object that will only store unique keys
+        const visitedNodes = new Set();
+        queue.push(startNode);
+        visitedNodes.add(startNode);
+        while (queue.length) {
+            //remove the first item from the queue
+            const currentNode = queue.shift();
+            // get all the neighbors of the node that I took off of the queue
+            const neighbors = this.getNeighbors(currentNode);
+            //loop over all neighbors
+            for (let neighbor of neighbors) {
+                const neighborNode = neighbor.vertex;
+                //if the set has the node I'm looking for
+                if (visitedNodes.has(neighborNode)) {
+                    continue;
+                } else {
+                    //else, if haven't been there, add to set
+                    visitedNodes.add(neighborNode);
+                }
+                //put it into the queue
+                queue.push(neighborNode);
+            }
+        }
+        //return the Set which contains all of the nodes in order visited
+        return visitedNodes;
+    }
+}
+
+module.exports = { Vertex, Edge, Graph };
